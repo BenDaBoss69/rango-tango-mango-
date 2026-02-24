f74645253ee48bc585417e917ff61b6e5a1a114b
Benjamin Ferenbach <bferenbach@gmail.com>
2026-02-24 21:25:58 +0000

    Delete rangovenv directory

--- rangovenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\benprograms\2ndsem\web\rango-tango-mango-\rangovenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
